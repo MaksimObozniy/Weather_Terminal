e28f6ff3e2ee4c17cb900442ee4709cb8f4bad21
MaksimObozniy <125642497+MaksimObozniy@users.noreply.github.com>
2024-09-06 18:06:11 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ def main():
     cities = ["london","svo","Череповец"]
     
     query_params = {'nMTqu': '', 'lang': 'ru'}
-    encoded_params = urllib.parse.urlencode(query_params)
     
     for city in cities:
         encoded_city = urllib.parse.quote(city)
-        url = f"{url_template.format(encoded_city)}?{encoded_params}"
-        response = requests.get(url)
-        print(response.text)
+        url = url_template.format(encoded_city)
+        response = requests.get(url, params=query_params)
+        status_code = response.status_code
+        if status_code == 200:
+            print(response.text)
+        else:
+            print(response.raise_for_status)
 
 
 if __name__ == "__main__":
